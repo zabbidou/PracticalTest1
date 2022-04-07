cf7a432ca8c7b51e40b7e618a498fec61577afa6
zabbidou <45122291+zabbidou@users.noreply.github.com>
2022-04-07 12:20:34 +0300

    Ex A, graphical interface

---
+++ PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/MainActivity.java
@@ -0,0 +1,16 @@
+package ro.pub.cs.systems.eim.practicaltest01var03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
